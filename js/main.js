b2ccb21e11e3002795b29f4f664f4c7708eb9a6e
uzmani <uzmani@yahoo.com>
2014-01-09 11:51:31 -0800

    second commit

---
+++ js/main.js
@@ -0,0 +1,85 @@
+$(document).ready(function(){
+  addBartender();
+  $("#total").validate({
+    rules: {
+      totaltips: {
+        required: true,
+        digits: true
+      }
+    }
+  });
+  $("form").each(function() {
+    $(this).validate({
+      rules: {
+        hours: {
+          required: true,
+          digits: true
+        }
+      }
+    });
+  });
+
+  $("#bartender-info").on('mouseover', renderRemoveButton)
+  $(".add-button").on("click", addBartender)
+  $(".calc-button").on("click", calcTips)
+});
+
+var bartenders = []
+
+function Bartender(firstname, hrs) {
+  this.firstName = firstname;
+  this.hrs = hrs;
+  this.tipsOwed = 0;
+}
+
+function addHours(){
+  var sumHours = 0
+  for(var i=0; i<bartenders.length; i++) {
+    var hoursInteger = bartenders[i].hrs
+    sumHours += parseFloat(hoursInteger);
+  }
+  return sumHours
+}
+
+function calcTips() { 
+  $('#name').remove();
+  bartenders = [];
+  createBartenders();
+  var totalTips = $('input[name="totaltips"]').val();
+  var hoursAdded = addHours(bartenders);
+  var rate = totalTips / hoursAdded;
+  for (var i=0; i<bartenders.length; i++) {  
+    bartenders[i].tipsOwed = Math.round((bartenders[i].hrs * rate)*100)/100
+  } 
+  renderResults();
+}
+
+function createBartenders() {
+  $('#bartender-to-add .bartender-info').each(function() {
+    var name = $(this).find('input').val();
+    var hrs = $(this).find('input').eq(1).val();
+    if (name === '' || hrs === '') {
+      return true; 
+    }else {
+      bartenders.push(new Bartender(name, hrs));
+    }
+  });
+}
+
+function addBartender(){
+  var addBartenderTemplate = $.trim($('#add-template').html());
+  $("#bartender-to-add").append(addBartenderTemplate);
+}
+
+function renderRemoveButton() {
+  console.log("moused over")
+}
+
+function renderResults(){
+  var source =  $('#render-results').html();
+  var template = Handlebars.compile(source);
+  var poolData = { people: bartenders }
+  $('body').append(template(poolData));
+}
+
+
